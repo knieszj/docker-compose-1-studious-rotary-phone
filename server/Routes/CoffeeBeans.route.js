e415fbcd68518ca0e9423362867393cf17e4ad52
Josef Kniesz <knieszj@jdi.socom.mil>
2021-01-19 07:21:22 -0500

    GET by id and GET all works.  DELETE by id also works

--- server/Routes/CoffeeBeans.route.js
+++ server/Routes/CoffeeBeans.route.js
@@ -5,12 +5,42 @@ const mongoose = require('mongoose')
 const CoffeeBeanSchema = require(`../MongoModels/CoffeeBeans.model`)
 
 router.get('/', ((req, res, next) => {
-    res.send(`getting a list of all the coffee beans....`)
+    CoffeeBeanSchema
+        .find()
+        .exec()
+        .then(docs => {
+            console.log("from the super cool MongoDB Atlas", docs);
+            if (docs.length > 0) {
+                res.status(200).json(docs)
+            } else if (docs.length === 0) {
+                // if length === 0, the database sent back an empty array and the connection is good but there is no data
+                res.status(204).json(docs)
+            }
+            // There is no else statement for if .length is less than 0 since it would be an error and caught there
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err})
+        })
+    // res.send(`getting a list of all the coffee beans....`)
 }))
 
-router.post(`/`, (req, res, next) => {
+router.get(`/:CoffeeBeanID`, (req, res, next) => {
+    const id = req.params.CoffeeBeanID;
+    CoffeeBeanSchema.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From the MongoDB Atlas database", doc)
+            res.status(200).json(doc)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: err})
+        })
+})
 
-    const coffeeBean = new CoffeeBeanSchema ({
+router.post(`/`, (req, res, next) => {
+    const coffeeBean = new CoffeeBeanSchema({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         weight: req.body.weight,
@@ -26,8 +56,34 @@ router.post(`/`, (req, res, next) => {
         message: `handling POST requests to /api/CoffeeBeans`,
         createdCoffeeBean: coffeeBean
     });
+})
 
-
+router.delete(`/:CoffeeBeanID`, (req, res, next) => {
+    const id = req.params.CoffeeBeanID;
+    CoffeeBeanSchema.remove({_id: id})
+        .exec()
+        .then(theResult => {
+            res.status(200).json(theResult)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
